4f0a7ef329c37212d103ec817de897aa9c743eb1
Eric Clavel <e23clavel@gmail.com>
2025-10-29 15:43:52 -0700

    feat(nodes): add Binary Mask  Weight Map (SAM/GDINO friendly)

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -12,3 +12,10 @@ NODE_DISPLAY_NAME_MAPPINGS = {}
 # Merge node maps (easy to extend as we add more nodes)
 NODE_CLASS_MAPPINGS.update(_SM_MAPPINGS)
 NODE_DISPLAY_NAME_MAPPINGS.update(_SM_DISPLAY)
+
+from .binary_mask_to_weightmap import (
+    NODE_CLASS_MAPPINGS as _BM_MAPPINGS,
+    NODE_DISPLAY_NAME_MAPPINGS as _BM_DISPLAY,
+)
+NODE_CLASS_MAPPINGS.update(_BM_MAPPINGS)
+NODE_DISPLAY_NAME_MAPPINGS.update(_BM_DISPLAY)
